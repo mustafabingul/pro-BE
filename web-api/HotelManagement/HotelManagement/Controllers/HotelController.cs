73f73cebcf8dc0bc4bc01d40c11e44f127fd2d10
Mustafa <m.bingul@gtu.edu.tr>
2022-05-21 12:55:00 +0200

    Add files via upload

---
+++ web-api/HotelManagement/HotelManagement/Controllers/HotelController.cs
@@ -0,0 +1,28 @@
+﻿using HotelManagement.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HotelManagement.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        [HttpGet(Name = "GetHotels")]
+        public IEnumerable<HotelInfo> Get()
+        {
+            //List<HotelInfo> list = new List<HotelInfo>();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = System.IO.Path.Combine(currentDirectory, "Data", "hotels.json");
+            string hotels = System.IO.File.ReadAllText(filePath);
+            //JArray jArray = JArray.Parse(hotels);
+            IEnumerable<HotelInfo> list = JsonConvert.DeserializeObject<IEnumerable<HotelInfo>>(hotels);
+            Console.WriteLine(list.ToString());
+            return list;
+        }
+    }
+}
---
+++ web-api/HotelManagement/HotelManagement/Model/HotelInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagement.Model
+{
+    public class HotelInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int Rate { get; set; }
+    }
+}
